75254fc20816d095417eb4c83984b4ab8a2f5df5
Memeng-04 <giangamir.umadhay-22@cpu.edu.ph>
2025-08-21 11:18:45 +0300

    main mernu

--- Godot script/Scenes Script/home_page.gd
+++ Godot script/Scenes Script/home_page.gd
@@ -1,1 +1,17 @@
 extends Control
+
+@onready var left_panel = $HBoxContainer/LeftPanel
+@onready var right_panel = $HBoxContainer/RightPanel
+@onready var middle_container = $HBoxContainer/MiddleContainer
+
+var left_visible := true
+var right_visible := true
+
+
+func _on_left_button_pressed() -> void:
+	left_visible = !left_visible
+	left_panel.visible = left_visible
+
+func _on_right_button_pressed() -> void:
+	right_visible = !right_visible
+	right_panel.visible = right_visible
