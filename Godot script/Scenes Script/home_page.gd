94d54e9b2f2b313c86a05f671db74447db1c0f3d
Memeng-04 <giangamir.umadhay-22@cpu.edu.ph>
2025-08-21 12:25:51 +0300

    added panel

--- Godot script/Scenes Script/home_page.gd
+++ Godot script/Scenes Script/home_page.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var left_panel = $HBoxContainer/LeftPanel
 @onready var right_panel = $HBoxContainer/RightPanel
 @onready var middle_container = $HBoxContainer/MiddleContainer
+@onready var shop_panel = $HBoxContainer/middle_container/ShopPanel
 
 var left_visible := true
 var right_visible := true
@@ -15,3 +16,6 @@ func _on_left_button_pressed() -> void:
 func _on_right_button_pressed() -> void:
 	right_visible = !right_visible
 	right_panel.visible = right_visible
+
+func _on_shop_button_button_down() -> void:
+	shop_panel.visible = true
