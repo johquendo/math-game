dce19e9cabf2a2377c28f1d7bd0ab00af4c8dd83
Memeng-04 <giangamir.umadhay-22@cpu.edu.ph>
2025-08-21 12:55:51 +0300

    panels sa hud

--- Godot script/Scenes Script/home_page.gd
+++ Godot script/Scenes Script/home_page.gd
@@ -4,10 +4,13 @@ extends Control
 @onready var right_panel = $HBoxContainer/RightPanel
 @onready var middle_container = $HBoxContainer/MiddleContainer
 @onready var shop_panel = $HBoxContainer/middle_container/ShopPanel
+@onready var shop_exit = $HBoxContainer/middle_container/ShopPanel/ShopButton
+@onready var inventory_panel = $HBoxContainer/middle_container/InventoryPanel
+@onready var leaderboards_panel = $HBoxContainer/MarginContainer/MarginContainer/VBoxContainer/LeaderboardsButton
+@onready var settings_panel = $HBoxContainer/middle_container/SettingsPanel
 
 var left_visible := true
 var right_visible := true
-
 
 func _on_left_button_pressed() -> void:
 	left_visible = !left_visible
@@ -19,3 +22,24 @@ func _on_right_button_pressed() -> void:
 
 func _on_shop_button_button_down() -> void:
 	shop_panel.visible = true
+
+func _on_shop_exit_button_down() -> void:
+	shop_panel.visible = false
+
+func _on_inventory_button_button_down() -> void:
+	inventory_panel.visible = true
+	
+func _on_inventory_exit_button_down() -> void:
+	inventory_panel.visible = false
+	
+func _on_leaderboards_button_button_down() -> void:
+	leaderboards_panel.visible = true
+	
+func _on_leaderboards_exit_button_down() -> void:
+	leaderboards_panel.visible = false
+
+func _on_settings_button_button_down() -> void:
+	settings_panel.visible = true
+	
+func _on_settings_exit_button_down() -> void:
+	settings_panel.visible = false
