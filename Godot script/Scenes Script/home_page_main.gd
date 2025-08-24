3ffc3d497fa4daf647c5900a96c26fe3c3d15a34
Your Name <bryan.delrosario-24@cpu.edu.ph>
2025-08-24 13:39:38 +0800

    finished all pop up ui

--- Godot script/Scenes Script/home_page_main.gd
+++ Godot script/Scenes Script/home_page_main.gd
@@ -6,13 +6,16 @@ extends Control
 @onready var shopui: Panel = $shopui
 @onready var countdown_label: Label = $shopui/CountdownLabel
 @onready var settings: Panel = $settings
+@onready var inventory: Panel = $inventory
+@onready var leaderboards: Panel = $leaderboards
+@onready var profile: Panel = $profile
 
 
 # Timer variables
 const DURATION := 10 * 60 # 10 minutes in seconds
 var remaining_time: float = DURATION
 
-# --- UI Logic ---
+# --- Pop-up Shop ---
 func _ready() -> void:
 	main.visible = true
 	shopui.visible = false
@@ -40,7 +43,8 @@ func _format_time(seconds: int) -> String:
 	return "%02d:%02d" % [minutes, secs]
 
 # --- Settings Ui ---
-func _readyy():
+
+func _ready_setting():
 	settings.visible = false
 	main.visible = true
 	
@@ -50,5 +54,50 @@ func _on_backk_button_down() -> void:
 func _on_settings_button_down() -> void:
 	settings.visible = true
 	
-func _on_backk_pressed() -> void:
-	_readyy()
+func _on_back_setting_pressed() -> void:
+	_ready_setting()
+
+# --- Pop-up Profile ---
+
+func _ready_profile():
+	profile.visible = false
+	main.visible = true
+	
+func _on_back_profile_button_down() -> void:
+	profile.visible = false
+	
+func _on_profile_button_down() -> void:
+	profile.visible = true
+	
+func _on_back_profile_pressed() -> void:
+	_ready_profile()
+
+# --- Pop-up Inventory ---
+
+func _ready_inventory():
+	inventory.visible = false
+	main.visible = true
+	
+func _on_inventory_button_down() -> void:
+	inventory.visible = true
+	
+func _on_back_inventory_button_down() -> void:
+	inventory.visible = false
+	
+func _on_back_inventory_pressed() -> void:
+	_ready_inventory()
+
+# --- Pop-up Leaderboards ---
+
+func _ready_leaderboards():
+	leaderboards.visible = false
+	main.visible = true
+	
+func _on_leaderboards_button_down() -> void:
+	leaderboards.visible = true
+	
+func _on_back_leaderboards_button_down() -> void:
+	leaderboards.visible = false
+	
+func _on_back_leaderboards_pressed() -> void:
+	_ready_leaderboards()
